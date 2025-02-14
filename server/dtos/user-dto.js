865767591d091a4914878712b320b530a05b7f89
Maxim <MaximK38136@yandex.ru>
2025-02-14 22:38:44 +0400

    fix: register logic (not full)

--- server/dtos/user-dto.js
+++ server/dtos/user-dto.js
@@ -1,9 +1,7 @@
 module.exports = class UserDto {
-  email
   id
   isActivated
   constructor(model) {
-    this.email = model.email
     this.id = model._id
     this.isActivated = model.isActivated
   }
