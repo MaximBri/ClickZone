166d4f241b04ace4e771d3632d6ffeff228e6c8f
Maxim <MaximK38136@yandex.ru>
2025-02-15 20:54:03 +0400

    fix: animals list

--- client/src/pages/home/ui/model/animalsList.ts
+++ client/src/pages/home/ui/model/animalsList.ts
@@ -11,7 +11,7 @@ export const animalsList: animalInterface[] = [
   { id: 4, name: 'Мармеладка', image_path: '4.png' },
   { id: 5, name: 'Черныш', image_path: '5.png' },
   { id: 6, name: 'Фокси', image_path: '6.png' },
-  { id: 7, name: 'Орешник (Орешек)', image_path: '7.png' },
+  { id: 7, name: 'Орешек', image_path: '7.png' },
   { id: 8, name: 'Тенька', image_path: '8.png' },
   { id: 9, name: 'Бэтта', image_path: '9.png' },
   { id: 10, name: 'Снежок', image_path: '10.png' },
