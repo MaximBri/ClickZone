eaf3037bcadec75cbc7a0e7a779da3c9b9b74673
Maxim <MaximK38136@yandex.ru>
2025-03-29 11:44:41 +0400

    feat(ui): add close-icons in auth&register pop ups

--- client/src/widgets/pop-ups/register/Register.tsx
+++ client/src/widgets/pop-ups/register/Register.tsx
@@ -1,5 +1,6 @@
-import { registerModel } from './model/registerModel';
-import styles from '../shared/Auth&Register.module.scss';
+import { registerModel } from "./model/registerModel";
+import crossSvg from "@/shared/icons/cross.svg";
+import styles from "../shared/Auth&Register.module.scss";
 
 export const Register = () => {
   const data = registerModel();
@@ -12,54 +13,61 @@ export const Register = () => {
       ></div>
       <section
         ref={data.refs.body}
-        className={`${styles.window} ${styles['window--animated']}`}
+        className={`${styles.window} ${styles["window--animated"]}`}
       >
         <h2 className={styles.window__title}>Регистрация</h2>
+        <button
+          onClick={() => data.closeRegisterWindow()}
+          className={styles["window__close-button"]}
+        >
+          <img src={crossSvg} alt="close" />
+        </button>
         <form className={styles.window__body} action={data.sendForm}>
           <h3
-            className={`${styles['window__body-error']} ${
-              data.error.item ? styles['window__body-error--active'] : ''
+            className={`${styles["window__body-error"]} ${
+              data.error.item ? styles["window__body-error--active"] : ""
             }`}
           >
             {data.error.item && data.error[data.error.item]}
           </h3>
-          <label className={styles['window__body-item']}>
+
+          <label className={styles["window__body-item"]}>
             Ваш логин
             <input
-              className={styles['window__body-input']}
+              className={styles["window__body-input"]}
               value={data.formData.login}
-              onChange={(e) => data.onChangeData('login', e.target.value)}
+              onChange={(e) => data.onChangeData("login", e.target.value)}
               type="text"
               placeholder="Логин, не менее 4 символов"
             />
           </label>
-          <label className={styles['window__body-item']}>
+          <label className={styles["window__body-item"]}>
             Ваш пароль
             <input
-              className={styles['window__body-input']}
+              className={styles["window__body-input"]}
               value={data.formData.pass}
-              onChange={(e) => data.onChangeData('pass', e.target.value)}
+              onChange={(e) => data.onChangeData("pass", e.target.value)}
               type="password"
               placeholder="Пароль, не менее 4 символов"
             />
           </label>
-          <label className={styles['window__body-item']}>
+          <label className={styles["window__body-item"]}>
             Повторите пароль
             <input
-              className={styles['window__body-input']}
+              className={styles["window__body-input"]}
               value={data.formData.repeatPass}
-              onChange={(e) => data.onChangeData('repeatPass', e.target.value)}
+              onChange={(e) => data.onChangeData("repeatPass", e.target.value)}
               type="password"
               placeholder="Повторите пароль"
             />
           </label>
-          <button className={styles['window__body-button']} type="submit">
-            {data.loading ? 'Отправка...' : 'Отправить'}
+          <button className={styles["window__body-button"]} type="submit">
+            {data.loading ? "Отправка..." : "Отправить"}
           </button>
         </form>
         <button
           onClick={() => data.openAuthWindow()}
-          className={styles['window__create-acc']}
+          className={styles["window__create-acc"]}
         >
           Есть аккаунт? <span>Войти</span>
         </button>
