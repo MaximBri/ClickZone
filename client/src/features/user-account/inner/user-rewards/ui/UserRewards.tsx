e880663ca1cef77c7f4541db4a7d4f258d5368a5
Maxim <MaximK38136@yandex.ru>
2025-04-01 08:54:05 +0400

    fix(ui): added tutorial for user rewards

--- client/src/features/user-account/inner/user-rewards/ui/UserRewards.tsx
+++ client/src/features/user-account/inner/user-rewards/ui/UserRewards.tsx
@@ -1,16 +1,24 @@
-import { useAppSelector } from "@/app/store/store";
+import { useAppDispatch, useAppSelector } from "@/app/store/store";
 import { UserReward } from "../user-reward";
+import infoSvg from "@/shared/icons/info.svg";
 import styles from "./UserRewards.module.scss";
+import { setTutorial } from "@/widgets/pop-ups/model/popUpsSlice";
 
 export const UserRewards = () => {
+  const dispatch = useAppDispatch();
   const rewardsList = useAppSelector(
     (state) => state.user.globals.achievements
   );
-  console.log(rewardsList);
 
   return (
     <section className={styles.rewards}>
       <h3 className={styles.rewards__title}>Награды</h3>
+      <button
+        className={styles.rewards__info}
+        onClick={() => dispatch(setTutorial({ key: "rewards", value: true }))}
+      >
+        <img src={infoSvg} alt="info" />
+      </button>
       <ul className={styles.rewards__list}>
         {rewardsList.map((reward) => {
           return <UserReward data={reward} key={reward.name} />;
