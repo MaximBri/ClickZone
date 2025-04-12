a1cdce4a2f35d4eca0445221e2e27e64ef47aab6
Maxim <MaximK38136@yandex.ru>
2025-04-12 17:59:30 +0400

    feat(ui): add adaptive for containers page

--- client/src/pages/randomizer/ui/RandomizePage.tsx
+++ client/src/pages/randomizer/ui/RandomizePage.tsx
@@ -1,20 +1,30 @@
 import { useAppDispatch, useAppSelector } from "@/app/store/store";
 
-import { containerList } from "../model/containerList";
+import { containerList, key } from "../model/containerList";
 import { setTutorial } from "@/widgets/pop-ups/model/popUpsSlice";
 import { Container } from "@/features/randomizer/container";
 import { OwnContainer } from "@/features/randomizer/own-container";
 import { Key } from "@/features/randomizer/key";
 import infoSvg from "@/shared/icons/info.svg";
 import styles from "./RandomizePage.module.scss";
+import { DOMAIN } from "@/shared/config/routes";
 
 export const RandomizePage = () => {
   const dispatch = useAppDispatch();
   const userContainers = useAppSelector((state) => state.containers.data);
+  const userKeys = useAppSelector((state) => state.containers.keys);
 
   return (
     <section className={styles.containers}>
       <h2 className={styles.containers__title}>Рандомайзер</h2>
+      <div className={styles.containers__key}>
+        {userKeys}
+        <img
+          className={styles.key__image}
+          src={`${DOMAIN}/images/containers/${key.imagePath}`}
+          alt="key"
+        />
+      </div>
       <button
         className={styles.containers__info}
         onClick={() =>
